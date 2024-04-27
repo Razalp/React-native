2d5fed1af5d5e30cb65b7bc9a3c80cdb57366a12
Razalp <razalp0012300@gmail.com>
2024-04-27 16:18:41 +0530

    image over lay on text

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View,Image,ImageBackground } from 'react-native';
 const logoImage =require("./assets/adaptive-icon.png")
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
+
+      {/* <Image className="h-80 w-32" source={logoImage}/>
+      <Image className="h-80 w-32 object-fill" source={{uri:"https://picsum.photos/300/300"}}/> */}
+      <ImageBackground source={logoImage}>
       <Text className="text-black text-5xl">Hello world</Text>
-      <Image className="h-80 w-32" source={logoImage}/>
-      <Image className="h-80 w-32 object-fill" source={{uri:"https://picsum.photos/300/300"}}/>
+      </ImageBackground>
 
       <StatusBar style="auto" />
     </View>
