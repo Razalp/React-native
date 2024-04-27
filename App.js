6d94abbd4ef5825e0f0f487b3d727c276cabc649
Razalp <razalp0012300@gmail.com>
2024-04-27 16:13:46 +0530

    image

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ const logoImage =require("./assets/adaptive-icon.png")
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
-      <Text className="text-black text-3xl">Hello world</Text>
+      <Text className="text-black text-5xl">Hello world</Text>
       <Image className="h-80 w-32" source={logoImage}/>
+      <Image className="h-80 w-32 object-fill" source={{uri:"https://picsum.photos/300/300"}}/>
+
       <StatusBar style="auto" />
     </View>
   );
