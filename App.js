3603ac2724d835520c7c714625025ee1748dc921
Razalp <razalp0012300@gmail.com>
2024-04-27 16:04:51 +0530

    Image

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+const logoImage =require("./assets/adaptive-icon.png")
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-     
-      <Text>
-      <Text className="text-white">Hello world</Text>
-      </Text>
-
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Text className="text-black text-3xl">Hello world</Text>
+      <Image className="h-80 w-32" source={logoImage}/>
       <StatusBar style="auto" />
     </View>
   );
